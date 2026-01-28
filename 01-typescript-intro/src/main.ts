ac16f442b08614b4d406d55c0b769255e0e56b87
Victor Arango <victorarangoquiroz@gmail.com>
2026-01-28 11:02:36 -0500

    desestructuracion de objectos

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 // import './topics/01-basic-types'
 // import './topics/02-object-interface'
 // import './topics/03-functions'
-import './topics/04-homework'
+// import './topics/04-homework'
+import './topics/05-basic-destructuring'
 
 const app  = document.querySelector<HTMLDivElement>('#app')!;
 
---
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -0,0 +1,30 @@
+interface AudioPlayer { 
+    audioVolume: number;
+    songDurartion: number;
+    song: string;
+    details:Details
+}
+
+
+interface Details{
+    author:string;
+    year:number;
+}
+
+const audioPlayer: AudioPlayer ={
+    audioVolume: 90,
+    songDurartion: 36,
+    song: "Mess",
+    details:{
+        author:"Ed Sheeran",
+        year:2015
+    }
+}
+const song = 'New Song';
+
+const {song:anotherSong,songDurartion:duration, details} = audioPlayer;
+const { author } =details;
+
+console.log('Song : ', anotherSong);
+console.log('Duration : ', duration);
+console.log('Author : ', author);
