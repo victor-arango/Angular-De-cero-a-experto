609c400e0f542ecbf882507319901f50d97abbfd
Victor Arango <victorarangoquiroz@gmail.com>
2026-01-28 10:35:29 -0500

    funciones con objectos como argumentos

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -14,15 +14,34 @@ function multiply( firsNumber: number, seconNumber?: number, base: number  = 2){
 }
 
 
-const result: number  = addNumbers( 1, 2 );
-const resul2: string  = addNumersArrow( 1, 2 );
-const multiplyResult: number  = multiply(5);
+// const result: number  = addNumbers( 1, 2 );
+// const resul2: string  = addNumersArrow( 1, 2 );
+// const multiplyResult: number  = multiply(5);
+// console.log({ result, resul2, multiplyResult });
 
-console.log({ result, resul2, multiplyResult });
+interface Character{
+    name: string;
+    hp: number;
+    showHp :() => void;
+}
+
+const healCharacter = (character: Character, amount:number )  =>{
+
+    character.hp += amount;
+}
 
 
 
+const strider:Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`puntos de vida ${this.hp}`);
+    },
+}
 
+healCharacter ( strider , 50);
+strider.showHp();
 
 
 
